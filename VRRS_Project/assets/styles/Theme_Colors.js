4e17421a1531790cb2f1833855bec4667344b745
Leeseoleem <leeseorim0029@gmail.com>
2024-08-25 13:56:06 +0900

    refactor-const-name-change
    변수명 변경

--- VRRS_Project/assets/styles/Theme_Colors.js
+++ VRRS_Project/assets/styles/Theme_Colors.js
@@ -1,6 +1,6 @@
 // 흑백 계열 색상을 정리해둔 코드입니다.
-// Gray.변수명 으로 사용할 수 있습니다.
-export const Gray = {
+// Gray_theme.변수명 으로 사용할 수 있습니다.
+export const Gray_theme = {
   balck: "#1B1B1B",
   gray_90: "#212121",
   gray_80: "#424242",
@@ -14,8 +14,8 @@ export const Gray = {
 };
 
 // 프로젝트 메인 색상을 정리해둔 코드입니다.
-// Main.변수명 으로 사용할 수 있습니다.
-export const Main = {
+// Main_theme.변수명 으로 사용할 수 있습니다.
+export const Main_theme = {
   main_10: "#DEF9C4",
   main_20: "#9CDBA6",
   main_30: "#30BC94",
