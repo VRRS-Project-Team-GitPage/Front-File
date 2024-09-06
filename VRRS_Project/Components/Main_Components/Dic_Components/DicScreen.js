1be21e60ca600552894f2343b734fb1eb320be9d
Leeseoleem <leeseorim0029@gmail.com>
2024-09-06 17:29:42 +0900

    design-scroll-type-btn1

--- VRRS_Project/Components/Main_Components/Dic_Components/DicScreen.js
+++ VRRS_Project/Components/Main_Components/Dic_Components/DicScreen.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState, useEffect, useContext } from "react";
-import { useWindowDimensions, StyleSheet } from "react-native";
+import { useWindowDimensions, StyleSheet, ScrollView } from "react-native";
 import {
   View,
   Text,
@@ -23,6 +23,7 @@ import {
 // style 관련 import
 import { Gray_theme, Main_theme } from "../../../assets/styles/Theme_Colors";
 import Octicons from "@expo/vector-icons/Octicons";
+import { vegTypes } from "../../../assets/ServerDatas/Dummy/dummyVegTypes";
 
 export default function DicScreen({ route, navigation }) {
   // 화면 크기를 저장한 변수
@@ -109,6 +110,10 @@ export default function DicScreen({ route, navigation }) {
     sortProducts(); // 옵션 선택 시마다 정렬
   }, [selectedOption]);
 
+  useEffect(() => {
+    checkTypeBtn(checked); // sortedProducts가 변경될 때마다 필터링
+  }, [sortedProducts]);
+
   const sortProducts = () => {
     let sortedList = [...products]; // 원본 배열을 복사하여 정렬
 
@@ -132,142 +137,204 @@ export default function DicScreen({ route, navigation }) {
     }
   };
 
+  // 유형 항목 체크에 관한 내용입니다.
+  const [checked, setChecked] = useState("전체");
+  // 선택한 버튼에 따라 필터된 유형을 저장합니다.
+  const [checkedFilterList, sethCeckedFilterList] = useState([]);
+
+  // 선택된 버튼에 따라 제품 리스트를 필터링 하는 함수
+  // btnType: 버튼 유형을 받아오는 변수
+  const checkTypeBtn = (btnType) => {
+    setChecked(btnType);
+
+    if (btnType === "전체") {
+      sethCeckedFilterList(sortedProducts);
+    } else {
+      const filteredList = sortedProducts.filter(
+        (product) => getVegTypeName(product.veg_type_id) === btnType
+      );
+      sethCeckedFilterList(filteredList);
+      console.log(checkedFilterList);
+    }
+  };
+
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.searchHeader}>
-        <Octicons
-          name="arrow-left"
-          size={24}
-          color={Gray_theme.gray_90}
-          style={{ marginRight: 16 }}
-          onPress={() => {
-            navigation.goBack();
-            setSearchText("");
-          }}
-        />
-        <TextInput
-          style={{
-            width: windowWidth - 72,
-            ...styles.searchTextInput,
-          }}
-          placeholder="검색어를 입력해주세요"
-          onChangeText={(text) => setSearchText(text)}
-          value={searchText}
-          onSubmitEditing={handleOnSubmitEditing}
-        />
-        <Octicons
-          name="x-circle-fill"
-          size={16}
-          color={Gray_theme.gray_50}
-          style={{
-            position: "absolute",
-            right: 40,
-          }}
-          onPress={() => {
-            setSearchText("");
-          }}
-        />
-      </View>
-      <View style={styles.firstHeader}>
-        <View style={{ flexDirection: "row", alignItems: "center" }}>
-          <Octicons name="check" size={16} color={Gray_theme.gray_40} />
-          <Text
-            style={{
-              marginLeft: 8,
-              color: Gray_theme.gray_40,
-              fontSize: 12,
-              fontFamily: "Pretendard-Medium",
-            }}
-          >
-            개인 사전
-          </Text>
-        </View>
-        <TouchableOpacity
-          onPress={toggleDropdown}
-          style={styles.firstBtn}
-          activeOpacity={0.8}
-        >
-          <Text style={styles.buttonText}>{selectedOption}</Text>
-          <Octicons name="chevron-down" size={16} color={Gray_theme.gray_80} />
-        </TouchableOpacity>
-
-        {isDropdownOpen && (
-          <View style={styles.dropdownList}>
-            <TouchableOpacity onPress={() => selectOption("등록순")}>
-              <View style={styles.dropdownItemContainer}>
-                <Text
-                  style={[
-                    styles.dropdownItem,
-                    selectedOption === "등록순" && styles.selectedOptionText,
-                  ]}
-                >
-                  등록순
-                </Text>
-                {selectedOption === "등록순" && (
-                  <Octicons name="check" size={12} color={Main_theme.main_30} />
-                )}
-              </View>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => selectOption("인기순")}>
-              <View style={styles.dropdownItemContainer}>
-                <Text
-                  style={[
-                    styles.dropdownItem,
-                    selectedOption === "인기순" && styles.selectedOptionText,
-                  ]}
-                >
-                  인기순
-                </Text>
-                {selectedOption === "인기순" && (
-                  <Octicons name="check" size={12} color={Main_theme.main_30} />
-                )}
-              </View>
-            </TouchableOpacity>
+      <TouchableWithoutFeedback onPress={closeDropdown}>
+        <View style={{ flex: 1 }}>
+          <View style={styles.searchHeader}>
+            <Octicons
+              name="arrow-left"
+              size={24}
+              color={Gray_theme.gray_90}
+              style={{ marginRight: 16 }}
+              onPress={() => {
+                navigation.goBack();
+                setSearchText("");
+              }}
+            />
+            <TextInput
+              style={{
+                width: windowWidth - 72,
+                ...styles.searchTextInput,
+              }}
+              placeholder="검색어를 입력해주세요"
+              onChangeText={(text) => setSearchText(text)}
+              value={searchText}
+              onSubmitEditing={handleOnSubmitEditing}
+            />
+            <Octicons
+              name="x-circle-fill"
+              size={16}
+              color={Gray_theme.gray_50}
+              style={{
+                position: "absolute",
+                right: 40,
+              }}
+              onPress={() => {
+                setSearchText("");
+              }}
+            />
           </View>
-        )}
-      </View>
-      <View></View>
-      <FlatList
-        style={{ paddingHorizontal: 8 }}
-        showsVerticalScrollIndicator={false}
-        data={sortedProducts}
-        keyExtractor={(item) => item.id.toString()} // 각 제품의 고유 키 설정
-        renderItem={({ item }) => {
-          // 제품의 유형을 저장하는 변수
-          const itemVegTypeName = getVegTypeName(item.veg_type_id);
-          // 버튼 여부와 제품의 유형을 비교하는 로직 추가하기
-          return (
-            <View style={styles.itemContainer}>
-              <Image source={{ uri: item.image_url }} style={styles.image} />
-
-              <View style={styles.textContainer}>
-                {/* 제품 이름, 카테고리, 원재료, 채식 유형 표시 */}
-                <View>
-                  <Text style={styles.name}>{item.name}</Text>
-                  <Text style={styles.category}>{item.category}</Text>
-                </View>
-                <View
-                  style={{
-                    flexDirection: "row",
-                    justifyContent: "space-between",
-                  }}
-                >
-                  <Text style={styles.vegType}>
-                    {itemVegTypeName}
-                    {/* 아이템의 채식 유형 이름 표시 */}
-                  </Text>
-                  <View>
-                    <View></View>
-                    <View></View>
-                  </View>
-                </View>
-              </View>
+          <View style={styles.firstHeader}>
+            <View style={{ flexDirection: "row", alignItems: "center" }}>
+              <Octicons name="check" size={16} color={Gray_theme.gray_40} />
+              <Text
+                style={{
+                  marginLeft: 8,
+                  color: Gray_theme.gray_40,
+                  fontSize: 12,
+                  fontFamily: "Pretendard-Medium",
+                }}
+              >
+                개인 사전
+              </Text>
             </View>
-          );
-        }}
-      />
+            <TouchableOpacity
+              onPress={toggleDropdown}
+              style={styles.firstBtn}
+              activeOpacity={0.8}
+            >
+              <Text style={styles.buttonText}>{selectedOption}</Text>
+              <Octicons
+                name="chevron-down"
+                size={16}
+                color={Gray_theme.gray_80}
+              />
+            </TouchableOpacity>
 
-      <View style={{ height: 60 }}></View>
+            {isDropdownOpen && (
+              <View style={styles.dropdownList}>
+                <TouchableOpacity onPress={() => selectOption("등록순")}>
+                  <View style={styles.dropdownItemContainer}>
+                    <Text
+                      style={[
+                        styles.dropdownItem,
+                        selectedOption === "등록순" &&
+                          styles.selectedOptionText,
+                      ]}
+                    >
+                      등록순
+                    </Text>
+                    {selectedOption === "등록순" && (
+                      <Octicons
+                        name="check"
+                        size={12}
+                        color={Main_theme.main_30}
+                      />
+                    )}
+                  </View>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => selectOption("인기순")}>
+                  <View style={styles.dropdownItemContainer}>
+                    <Text
+                      style={[
+                        styles.dropdownItem,
+                        selectedOption === "인기순" &&
+                          styles.selectedOptionText,
+                      ]}
+                    >
+                      인기순
+                    </Text>
+                    {selectedOption === "인기순" && (
+                      <Octicons
+                        name="check"
+                        size={12}
+                        color={Main_theme.main_30}
+                      />
+                    )}
+                  </View>
+                </TouchableOpacity>
+              </View>
+            )}
+          </View>
+          <View>
+            <ScrollView
+              horizontal={true}
+              showsHorizontalScrollIndicator={false}
+              style={{ flexDirection: "row" }}
+            >
+              {vegTypes
+                .map((vegType) => vegType.name)
+                .map((btnType) => (
+                  <TouchableOpacity
+                    key={btnType}
+                    onPress={() => {
+                      checkTypeBtn(btnType);
+                    }}
+                  >
+                    <Text>{btnType}</Text>
+                  </TouchableOpacity>
+                ))}
+            </ScrollView>
+          </View>
+          <View style={{ flexGorw: 1 }}>
+            <FlatList
+              style={{ paddingHorizontal: 8 }}
+              showsVerticalScrollIndicator={false}
+              data={checkedFilterList}
+              keyExtractor={(item) => item.id.toString()} // 각 제품의 고유 키 설정
+              renderItem={({ item }) => {
+                // 제품의 유형을 저장하는 변수
+                const itemVegTypeName = getVegTypeName(item.veg_type_id);
+                // 버튼 여부와 제품의 유형을 비교하는 로직 추가하기
+                return (
+                  <View style={styles.itemContainer}>
+                    <Image
+                      source={{ uri: item.image_url }}
+                      style={styles.image}
+                    />
+
+                    <View style={styles.textContainer}>
+                      {/* 제품 이름, 카테고리, 원재료, 채식 유형 표시 */}
+                      <View>
+                        <Text style={styles.name}>{item.name}</Text>
+                        <Text style={styles.category}>{item.category}</Text>
+                      </View>
+                      <View
+                        style={{
+                          flexDirection: "row",
+                          justifyContent: "space-between",
+                        }}
+                      >
+                        <Text style={styles.vegType}>
+                          {itemVegTypeName}
+                          {/* 아이템의 채식 유형 이름 표시 */}
+                        </Text>
+                        <View>
+                          <View></View>
+                          <View></View>
+                        </View>
+                      </View>
+                    </View>
+                  </View>
+                );
+              }}
+            />
+          </View>
+          <View style={{ height: 60 }}></View>
+        </View>
+      </TouchableWithoutFeedback>
     </SafeAreaView>
   );
 }
