9411c472301a4dc805b226cc45bf473ff843f2e5
Leeseoleem <leeseorim0029@gmail.com>
2024-09-09 19:01:36 +0900

    feat-key-word-screen-1
    키워드로 추천받기 페이지 제작중입니다.

--- VRRS_Project/Components/Main_Components/Recommend_Components/Rec_CateScreen.js
+++ VRRS_Project/Components/Main_Components/Recommend_Components/Rec_CateScreen.js
@@ -1,9 +1,52 @@
 import { View, Text } from "react-native";
+import { StyleSheet, useWindowDimensions } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Gray_theme } from "../../../assets/styles/Theme_Colors";
+import Octicons from "@expo/vector-icons/Octicons";
 
-export default function Rec_CateScreen() {
+export default function Rec_CateScreen({ navigation }) {
+  // 화면 크기를 저장한 변수
+  const windowWidth = useWindowDimensions().width;
+  const windowHeigh = useWindowDimensions().height;
   return (
-    <View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <Octicons
+          name="arrow-left"
+          size={24}
+          color={Gray_theme.gray_90}
+          style={{
+            position: "absolute",
+            left: 24,
+          }}
+          onPress={() => {
+            navigation.goBack();
+          }}
+        />
+        <Text
+          style={{
+            textAlign: "center",
+            fontFamily: "Pretendard-Bold",
+          }}
+        >
+          유형으로 추천받기
+        </Text>
+      </View>
       <Text>RecommendScreen</Text>
-    </View>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Gray_theme.white,
+  },
+  header: {
+    height: 60,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    paddingHorizontal: 24,
+  },
+});
--- VRRS_Project/Components/Main_Components/Recommend_Components/Rec_SearchScreen.js
+++ VRRS_Project/Components/Main_Components/Recommend_Components/Rec_SearchScreen.js
@@ -1,9 +1,66 @@
 import { View, Text } from "react-native";
+import { StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Gray_theme } from "../../../assets/styles/Theme_Colors";
+import Octicons from "@expo/vector-icons/Octicons";
 
-export default function Rec_SearchScreen() {
+export default function Rec_SearchScreen({ navigation }) {
   return (
-    <View>
-      <Text>RecommendScreen</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <Octicons
+          name="arrow-left"
+          size={24}
+          color={Gray_theme.gray_90}
+          style={{
+            position: "absolute",
+            left: 24,
+          }}
+          onPress={() => {
+            navigation.goBack();
+          }}
+        />
+        <Text
+          style={{
+            textAlign: "center",
+            fontFamily: "Pretendard-Bold",
+          }}
+        >
+          키워드로 추천받기
+        </Text>
+      </View>
+      <View>
+        <View style={styles.titleContainer}>
+          <Text style={styles.title}>먹고 싶은</Text>
+          <View style={{ flexDirection: "row" }}>
+            <Text style={styles.title}>제품 키워드</Text>
+            <Text style={styles.title}>를</Text>
+          </View>
+          <Text style={styles.title}>입력해주세요.</Text>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Gray_theme.white,
+  },
+  header: {
+    height: 60,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    paddingHorizontal: 24,
+  },
+  titleContainer: {
+    marginVertical: 32,
+    paddingHorizontal: 24,
+  },
+  title: {
+    fontFamily: "Pretendard-SemiBold",
+    fontSize: 24,
+  },
+});
